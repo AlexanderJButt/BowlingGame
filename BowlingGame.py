2d8c01f8738f821be07fbddf39ffd6d8782a6689
AlexanderJButt <axjbutt@gmail.com>
2023-06-28 18:02:06 +1200

    created doctags and added extra tests also fixed bug in test and main
    doc tags created for BowlingGame.py
    3 new tests created
    -testing one strike followed by a spear
    -testing testing a strike followed by 2  misses then a strike.
    -testing three strikes at the end of the game.
    bugs fixed
    -added a function to roll a strike so that there will be a 0 after the strike in the scoreBoard.
    -fixed the way strikes are calculated.(acounting for the zero after the strike.

--- BowlingGame.py
+++ BowlingGame.py
@@ -2,13 +2,25 @@
 #This file has information about Bowling Game for which the description is provided in project assessment.
 
 class BowlingGame:
+    """BowlingGame Class
+    
+        this class takes integers(repersenting the number of pins knocked down in a bowl) assigns them to an array
+        and then calculates the score based on the rules of bowling"""
+    
     def __init__(self):
         self.scoreBoard=[]
+        '''Creates scoreBoard array to store the bowls of a player.'''
 
     def rolls(self,pins):
         self.scoreBoard.append(pins)
+        '''Rolls function
+        
+        Appends bowl(number of pins knocked down in one bowl) to the scoreBoard array.'''
 
     def score(self):
+        '''Score function
+        
+        calculates score based on the scoreBoard by following the rules of bowling '''
         result = 0
         rollIndex=0
         for frameIndex in range(10):
@@ -24,18 +36,33 @@ class BowlingGame:
         return result
 
     def isStrike(self, rollIndex):
+        '''isStrike function
+        
+            checks to see if the bowl made was a strike'''
         return self.scoreBoard[rollIndex] == 10
     
     def isSpare(self, rollIndex):
+        '''isSpare function
+        
+            checks to see if the bowls made was a spare'''
         return self.scoreBoard[rollIndex]+ self.scoreBoard[rollIndex+1]==10
     
     def strikeScore(self,rollIndex):
-        return  10+ self.scoreBoard[rollIndex+1]+ self.scoreBoard[rollIndex+2]
-
+        '''strikeScore function
+        
+            calculates the score when a strike has been bowled'''
+        return  10+ self.scoreBoard[rollIndex+2]+ self.scoreBoard[rollIndex+3]
+        
     def spareScore(self,rollIndex):
+        '''spearScore function
+        
+            calculates the score when a spare has been bowled'''
         return  10+ self.scoreBoard[rollIndex+2]
 
     def frameScore(self, rollIndex):
+        '''frameScore function
+        
+            calculates the score when neither a strike or spear have been bowled'''
         return self.scoreBoard[rollIndex] + self.scoreBoard[rollIndex + 1]
 		
 
@@ -46,4 +73,4 @@ class BowlingGame:
 
 
 ###### Important #####
-# Please complete your project and all tasks according to assessment description provided in CANVAS.
+# Please complete your project and all tasks according to assessment description provided in CANVAS. python -m pydoc -w BowlingGame
--- Test.py
+++ Test.py
@@ -1,5 +1,4 @@
 #File 1 (Test.py)
-#This file has information about test cases which you need to test.
 
 import unittest
 import BowlingGame
@@ -17,19 +16,38 @@ class TestBowlingGame(unittest.TestCase):
         self.rollMany(1, 20)
         assert self.game.score()==20
     def testOneSpare(self):
-        self.game.rolls(5)
-        self.game.rolls(5)
+        self.game.rolls(1)
+        self.game.rolls(9)
         self.game.rolls(3)
         self.rollMany(0,17)
         assert self.game.score()==16
     def testOneStrike(self):
-        self.game.rolls(10)
+        self.rollStrikes(1)
         self.game.rolls(4)
         self.game.rolls(3)
         self.rollMany(0,16)
         assert  self.game.score()==24
+    def testOneStrikeAndOneSpear(self):
+        self.rollStrikes(1)
+        self.game.rolls(9)
+        self.game.rolls(1)
+        self.rollMany(0,16)
+        assert  self.game.score()==30
+    def testStrikeMissStrike(self):
+        self.rollStrikes(1)
+        self.game.rolls(0)
+        self.game.rolls(0)
+        self.rollStrikes(1)
+        self.rollMany(0,16)
+        assert  self.game.score()==20
+    def testThreeStrikesAtEnd(self):
+        self.rollMany(0,18)
+        self.rollStrikes(3)
+        assert  self.game.score()==30
     def testPerfectGame(self):
-        self.rollMany(10,12)
+        self.rollStrikes(10)
+        self.game.rolls(10)
+        self.game.rolls(10)
         assert self.game.score()==300
     def testSpareGame(self):
         self.rollMany(5,21)
@@ -37,3 +55,7 @@ class TestBowlingGame(unittest.TestCase):
     def rollMany(self, pins,rolls):
         for i in range(rolls):
             self.game.rolls(pins)
+    def rollStrikes(self,rolls):
+        for i in range(rolls):
+            self.game.rolls(10)
+            self.game.rolls(0)
